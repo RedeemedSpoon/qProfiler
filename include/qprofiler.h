947d48a2f14a5415df3cb5ee9aefc0fa3f4cc1db
Ethan <QustoTemmo@protonmail.com>
2025-11-12 12:22:09 +0100

    Cmake Init

---
+++ include/qprofiler.h
@@ -0,0 +1,9 @@
+#ifndef QPROFILER_H
+#define QPROFILER_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#define VERSION "0.0.1"
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "qprofiler.h"
+
+int main(int argc, char *argv[]) {
+  printf("it is working!");
+  return 0;
+}
