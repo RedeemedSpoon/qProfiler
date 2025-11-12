afa7e3dd0fa9f701331ba44cb8e000d39c1f03d2
Ethan <QustoTemmo@protonmail.com>
2025-11-12 13:13:57 +0100

    Main C File

--- include/qprofiler.h
+++ include/qprofiler.h
@@ -6,4 +6,21 @@
 
 #define VERSION "0.0.1"
 
+typedef struct {
+  int _;
+} Options;
+
+typedef struct {
+  int _;
+} RawResults;
+
+typedef struct {
+  int _;
+} Results;
+
+Options parse_args(int argc, char *argv[]);
+RawResults monitor(Options options);
+Results aggregate_data(RawResults results);
+void show_results(Results results);
+
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
 #include "qprofiler.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("it is working!");
+  if (argc >= 1) {
+    printf("Usage: qprofiler [options] <shell_command>");
+    printf("Try 'qprofiler --help' for more information.");
+    exit(1);
+  }
+
+  Options options = parse_args(argc, argv);
+  RawResults raw_results = monitor(options);
+  Results results = aggregate_data(raw_results);
+  show_results(results);
+
   return 0;
 }
