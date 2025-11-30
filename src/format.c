7f6e5403b7bcbb7cf4e1634f5e49deef0411251e
Ethan <QustoTemmo@protonmail.com>
2025-11-30 18:44:35 +0100

    Output Functions

--- src/format.c
+++ src/format.c
@@ -1,7 +1,24 @@
 #include "qprofiler.h"
+#include <string.h>
+
+static char *to_text(Results *result) {
+  (void)result;
+  return "hello world";
+}
+
+static char *to_json(Results *result) {
+  (void)result;
+  return "hello world";
+}
+
+static char *to_csv(Results *result) {
+  (void)result;
+  return "hello world";
+}
 
 char *format_output(Results *results, char *format) {
-  (void)results;
-  (void)format;
-  return "...";
+  if (strcmp(format, "csv")) return to_csv(results);
+  else if (strcmp(format, "json")) return to_json(results);
+  else return to_text(results);
 }
+
--- src/output.c
+++ src/output.c
@@ -1,8 +1,25 @@
 #include "qprofiler.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 void output_results(Results *results, Options *options) {
   char *output = format_output(results, options->format_style);
-  (void)output;
-  (void)results;
-  (void)options;
+
+  if (options->output_file != NULL) {
+    char *file_options = options->append_flag ? "a" : "w";
+    char *output_file = options->output_file;
+
+    FILE *fptr = fopen(output_file, file_options);
+    if (fptr == NULL) {
+      print_error("failed to open output file");
+      exit(EXIT_FAILURE);
+    }
+
+    fprintf(fptr, "%s\n", output);
+    fclose(fptr);
+    exit(0);
+  }
+
+  fprintf(stderr, "%s\n", output);
+  exit(0);
 }
